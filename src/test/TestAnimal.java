87b72d281f85872c7d6c517e18e7f4739a8bf85d
888Daniel888 <58972003+888Daniel888@users.noreply.github.com>
2021-02-04 11:29:08 +0300

    Add files via upload

--- src/test/TestAnimal.java
+++ src/test/TestAnimal.java
@@ -5,11 +5,11 @@ import domain.Cat;
 public class TestAnimal {
 
     public static void main(String[] args) {
-        Cat cat = new Cat("Thomas");
+        Cat cat = new Cat("Genri");
         System.out.println(cat);
         cat.hunt();
         cat.eat();
         cat.speak();
         cat.play();
     }
-}
+} 
